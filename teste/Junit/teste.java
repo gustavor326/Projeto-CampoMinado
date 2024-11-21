2538d0ced417695bb2b9ed40a97843def7784e91
gustavor326 <gustavor326@gmail.com>
2024-11-21 11:18:43 -0300

    Adicionando Vizinhos

---
+++ teste/Junit/teste.java
@@ -0,0 +1,14 @@
+package Junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class teste {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
